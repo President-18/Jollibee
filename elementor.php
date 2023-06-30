c8692025a807c8062744fe30b1247dc712816ed8
President-18 <diegocantre007@gmail.com>
2023-06-30 12:50:07 +0800

    elementor

---
+++ elementor.php
@@ -0,0 +1,14 @@
+<?php
+    /* Template Name: Elementor*/
+
+    
+?>
+
+<?php get_header() ?>
+
+<?php if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'page' ) ) {
+    the_content();
+} ?>
+
+
+<?php get_footer() ?>
